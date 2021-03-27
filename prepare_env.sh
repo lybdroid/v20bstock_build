5c82b8e8e6b1477351af74501936f80f2af97ba2
lybxlpsv <lybxlpsv@yahoo.com>
2021-03-27 07:20:02 +0700

    fix build

--- prepare_env.sh
+++ prepare_env.sh
@@ -3,7 +3,7 @@
 sudo apt install bc
 sudo npm install -g git-credential-env
 
-it config credential.helper "env --username=GIT_USER --password=GIT_PASS"
+git config credential.helper "env --username=GIT_USER --password=GIT_PASS"
 
 git clone --depth=1 https://github.com/lybxlpsv/V20-Kernel-LYB.git -b develop-lts kernel
 git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b lld-integration
