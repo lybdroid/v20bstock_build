9955f4a646bf793683693c1ea42553062eadd463
lybxlpsv <lybxlpsv@yahoo.com>
2021-03-27 07:25:09 +0700

    fix build

--- prepare_env.sh
+++ prepare_env.sh
@@ -2,8 +2,6 @@
 
 sudo apt install bc
 
-git config credential.helper "!f() { echo \"username=${GIT_USER}\\npassword=${GIT_PASS}\"; }; f"
-
-git clone --depth=1 https://github.com/lybxlpsv/V20-Kernel-LYB.git -b develop-lts kernel
+git clone --depth=1 https://$GIT_USER:$GIT_PASS@github.com/lybxlpsv/V20-Kernel-LYB.git -b develop-lts kernel
 git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b lld-integration
 git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b lld-integration
