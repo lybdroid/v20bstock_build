6238a2104fdfd99a613a55b0c741257d24e41716
lybxlpsv <lybxlpsv@yahoo.com>
2021-05-12 10:45:01 +0700

    drone ci pt1

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y build-essential bc python curl git zip ftp libssl-dev lftp zstd wget libfl-dev flex bison cpio device-tree-compiler
-
-git clone --depth=1 https://$GIT_USER:$GIT_PASS@github.com/$repo_1 -b $branch_1 kernel
+git clone --depth=1 https://$GIT_USER:$GIT_PASS@github.com/$LOC1 -b $BR1 kernel
