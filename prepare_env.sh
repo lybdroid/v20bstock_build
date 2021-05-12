daf182042457790544781471c4b6cb6169401737
lybxlpsv <lybxlpsv@yahoo.com>
2021-05-12 10:39:34 +0700

    repo_1

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo apt install bc
+sudo apt-get update
+sudo apt-get install -y build-essential bc python curl git zip ftp libssl-dev lftp zstd wget libfl-dev flex bison cpio device-tree-compiler
 
-git clone --depth=1 https://$GIT_USER:$GIT_PASS@github.com/lybxlpsv/V20-Kernel-LYB.git -b develop-lts kernel
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b lld-integration
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b lld-integration
+git clone --depth=1 https://$GIT_USER:$GIT_PASS@github.com/$repo_1 -b $branch_1 kernel
--- variants_build.sh
+++ variants_build.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
 cd kernel
-
-CURR_PATH=${0%/*}
-CURR_PATH="$(realpath $CURR_PATH)/"
-
-mkdir -p "${CURR_PATH}/release"
-
-variants="H990DS"
-GCC_VER=$(./gcc64/bin/aarch64-elf-gcc --version | head -n 1)
-
-for variant in $variants
-do
-    ./build.sh $variant
-    ./copy_finished.sh
-    cd out
-    curl -F document=@H990DS_v2.0-BETA1-v20bcustom.zip "https://api.telegram.org/bot$BOTTOKEN/sendDocument" \
-        -F chat_id="$CHATID" \
-        -F caption="[BOT] Build for ${variant} v20bcustom ${GCC_VER}"
-
-done
+./kramel.sh
 
