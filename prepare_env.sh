6467d0586765f618e082b49555ee3e85797e14b5
lybxlpsv <lybxlpsv@users.noreply.github.com>
2021-05-29 11:19:35 +0700

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-git clone --depth=1 https://$GIT_USER:$GIT_PASS@github.com/$LOC1 -b $BR1 kernel
+apt update && apt-get install -y git &
+git clone --depth=1 https://$GIT_USER:$GIT_PASS@github.com/$LOC1 -b $BR1 kernel &
+wait
