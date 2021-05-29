f5ded4181b87a80e01b750a4d6ef0989ca6eacf4
lybxlpsv <lybxlpsv@users.noreply.github.com>
2021-05-29 11:20:17 +0700

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-apt update && apt-get install -y git &
+apt update && apt-get install -y git
+
+apt-get install -y make python2 bc python curl git zip ftp libssl-dev lftp zstd wget libfl-dev flex bison cpio device-tree-compiler &
 git clone --depth=1 https://$GIT_USER:$GIT_PASS@github.com/$LOC1 -b $BR1 kernel &
 wait
