b66542cbc230f0f161b81616d33dae40c5409134
lybxlpsv <lybxlpsv@users.noreply.github.com>
2021-07-20 04:23:32 +0700

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-apt install gpgv
-apt update && apt-get install -y git
+apt install -y git
 
 apt-get install -y make python2 bc python curl git zip ftp libssl-dev lftp zstd wget libfl-dev flex bison device-tree-compiler &
 git clone --depth=1 https://$GIT_USER:$GIT_PASS@github.com/$LOC1 -b $BR1 kernel &
