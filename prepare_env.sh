0e1ff85d8acda1895c09aa2e286fa67db044f425
lybxlpsv <lybxlpsv@users.noreply.github.com>
2021-07-20 04:30:28 +0700

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-apt-get install gnupg
-apt update
-apt install -y git
-
-apt-get install -y make python2 bc python curl git zip ftp libssl-dev lftp zstd wget libfl-dev flex bison device-tree-compiler &
-git clone --depth=1 https://$GIT_USER:$GIT_PASS@github.com/$LOC1 -b $BR1 kernel &
+curl -fsSL "https://repo.archlinuxcn.org/x86_64/glibc-linux4-2.33-4-x86_64.pkg.tar.zst" | bsdtar -C / -xvf - &>/dev/null
+pacman -Syu --needed --noconfirm wget base-devel xmlto inetutils bc bison cpio git zip unzip python2 dtc
 wait
