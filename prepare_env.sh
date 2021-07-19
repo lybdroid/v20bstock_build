cf53a59355ecb62801ffc62542ba2a7ee01c51ab
lybxlpsv <lybxlpsv@users.noreply.github.com>
2021-07-20 04:36:39 +0700

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 curl -fsSL "https://repo.archlinuxcn.org/x86_64/glibc-linux4-2.33-4-x86_64.pkg.tar.zst" | bsdtar -C / -xvf - &>/dev/null
-pacman -Syu --needed --noconfirm wget base-devel xmlto inetutils bc bison cpio git zip unzip python2 dtc &
-git clone --depth=1 https://$GIT_USER:$GIT_PASS@github.com/$LOC1 -b $BR1 kernel &
-wait
+pacman -Syu --needed --noconfirm wget base-devel xmlto inetutils bc bison cpio git zip unzip python2 dtc 
+git clone --depth=1 https://$GIT_USER:$GIT_PASS@github.com/$LOC1 -b $BR1 kernel 
